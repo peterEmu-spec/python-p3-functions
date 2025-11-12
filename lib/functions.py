127f51c6755a8c13845a929e73d737912ca57252
Peter Emu <emu.kimani@strathmore.edu>
2025-11-12 21:02:58 +0300

    lab done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print ("Hello, programmer!")
     pass
 
 def greet(name):
+    print (f"Hello, {name}!")
     pass
+greet ("Guido")
 
 def greet_with_default(name="programmer"):
+    print (f"Hello, {name}!")
     pass
 
 def add(num1, num2):
+    return (num1 + num2)
     pass
-
+add (45,55)
 def halve(number):
+    return number/2
     pass
